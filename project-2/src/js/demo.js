38a763e87af7a017f961c3ad20386943f946c8a5
Oplano James Mulbah <mulbahjamesoplano@gmail.com>
2026-09-10 22:41:12 +0200

    Delete project-2 directory

--- project-2/src/js/demo.js
+++
@@ -1,8 +0,0 @@
-const skills = ["HTML", "CSS", "PYTHON", "JAVASCRIPT", "PHP", "REACT"];
-const ul = document.querySelector("ul");
-
-for (const skill of skills) {
-  const li = document.createElement("li");
-  li.textContent = skill;
-  ul.appendChild(li);
-}
