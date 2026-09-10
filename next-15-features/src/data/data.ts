a9b5908aef7bccd3f4609565d0f03088761bf499
Oplano James Mulbah <mulbahjamesoplano@gmail.com>
2026-09-10 22:40:37 +0200

    Delete next-15-features directory

--- next-15-features/src/data/data.ts
+++
@@ -1,7 +0,0 @@
-export const navLinks = [
-  { label: "Home", path: "/" },
-  { label: "Products", path: "/products" },
-  { label: "Complex Dashboard", path: "/complex-dashboard" },
-  { label: "Services", path: "/services" },
-  { label: "Contact", path: "/contact" },
-];
